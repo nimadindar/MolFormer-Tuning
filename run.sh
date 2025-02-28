50cbbdb66b2cdeb69054538d9bafbc6f04a2e917
nimadindar <nima.dindarsafa@gmail.com>
2025-02-28 12:08:30 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nnti  # Change 'nnti' to your actual environment name
 
 # Upgrade pip and install dependencies
-python -m pip install --upgrade pip
+# python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 # Run your script
-python ~/NNTI_project/scripts/Task2.py
+# python ~/NNTI_project/scripts/Task2.py
