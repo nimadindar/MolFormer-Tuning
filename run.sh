d30cf2bcf41a80d9b0ab241f3f415423b5297150
nimadindar <nima.dindarsafa@gmail.com>
2025-02-28 11:40:34 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
 nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
 echo $HOSTNAME
 which python
 python -m pip list
 
+# Activate Conda environment (if using Conda)
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate nnti  # Change 'nnti' to your actual environment name
+
+# Upgrade pip and install dependencies
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+# Run your script
 python scripts/Taks2.py
